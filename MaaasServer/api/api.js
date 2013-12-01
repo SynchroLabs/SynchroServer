c4f0d7af4b45ba8b9d31a999889a1531a4d650fc
BobDickinson <bob.dickinson@gmail.com>
2013-12-01 01:24:20 -0800

    Implemented websocket support.

--- MaaasServer/api/api.js
+++ MaaasServer/api/api.js
@@ -11,7 +11,6 @@ for (var routePath in routes) {
     route.View["path"] = routePath;
 }
 
-
 function getObjectProperty(obj, propertyPath)
 {
     propertyPath = propertyPath.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
@@ -62,106 +61,9 @@ function setObjectProperty(obj, propertyPath, value)
     }
 }
 
-function processPath(path, request, response)
+function fnShowMessage(context, messageBox)
 {
-    console.log("Processing path " + path);
-    var context =
-    {
-        path: path,
-        request: request,
-        response: response,
-    }
-
-	var routeModule = routes[path];
-    if (routeModule)
-    {
-        var boundItemsAfterUpdate = null;
-
-        console.log("Found route module for " + path);
-
-        if (request.body && request.body.BoundItems)
-        {
-            console.log("BoundItems: " + request.session.BoundItems);
-
-            // Record the current state of bound items so we can diff it after apply the changes from the client,
-            // and use that diff to see if there were any changes, so that we can then pass them to the OnChange
-            // handler (for the "view" mode, indicating changes that were made by/on the view).
-            //
-            var boundItemsBeforeUpdate = JSON.parse(JSON.stringify(request.session.BoundItems));
-
-            // Now apply the changes from the client...
-            for (var key in request.body.BoundItems)
-            {
-                console.log("Request body bound item change from client - " + key + ": " + request.body.BoundItems[key]);
-                setObjectProperty(request.session.BoundItems, key, request.body.BoundItems[key]);
-            }
-
-            // Getting this here allows us to track any changes made by server logic (in change notifications or commands)
-            //
-            boundItemsAfterUpdate = JSON.parse(JSON.stringify(request.session.BoundItems));
-
-            // If we had changes from the view and we have a change listener for this route, call it.
-            if (routeModule.OnChange)
-            {
-                // !!! Pass changelist (consistent with command side changelist)
-                routeModule.OnChange(context, request.session, request.session.BoundItems, "view");
-            }
-        }
-
-        var command = request.query["command"]
-		if (command)
-        {
-            console.log("Running command: " + command);
-
-            // If no bound item updates happened on the client, we need to record the state of the
-            // bound items now, before we run any commands, so we can diff it after...
-            // (this is really "BoundItems after update from client, if any" or "BoundItems before
-            // any server code gets a crack at them).
-            //
-            if (!boundItemsAfterUpdate)
-            {
-                boundItemsAfterUpdate = JSON.parse(JSON.stringify(request.session.BoundItems));
-            }
-
-            if (!routeModule.Commands[command](context, request.session, request.session.BoundItems))
-            {
-                // !! This is a problem for non-default returns that route to the same page, such as MessageBox,
-                //    which also needs to get the bound item update...
-                //
-                // If command returns false (default), it has not created a response, so we
-                // create the default response (updating the bound items)...
-                console.log("Default command processing, returning bound items");
-
-                // If we have a change listener for this route, analyze changes, and call it as appropriate.
-                if (routeModule.OnChange)
-                {
-                    // !!! We might need to call getChangeList here also, to determine if there were any changes,
-                    //     and to construct the changelist for the handler.
-                    // !!! Pass changelist (consistent with view side changelist)
-                    routeModule.OnChange(context, request.session, request.session.BoundItems, "command");
-                }
-
-                var boundItemUpdates = objectMonitor.getChangeList(null, boundItemsAfterUpdate, request.session.BoundItems);
-                response.send({ "BoundItemUpdates": boundItemUpdates });
-            }
-        }
-        else
-        {
-            request.session.BoundItems = {};
-            if (routeModule.InitializeViewModelState)
-            {
-                console.log("Initializing view model state");
-                request.session.BoundItems = routeModule.InitializeViewModelState(context, request.session);
-            }
-            response.send({ "BoundItems": request.session.BoundItems, "View": routeModule.View });
-        }
-    }
-}
-
-function fnShowMessage(state, messageBox)
-{
-    state.response.send({ "BoundItems": state.request.session.BoundItems, "MessageBox": messageBox });
-    return true;
+    context.response.MessageBox = messageBox;
 }
 showMessage = fnShowMessage;
 
@@ -171,26 +73,117 @@ function fnNavigateToView(context, route)
     if (routeModule)
     {
         console.log("Found route module for " + route);
-        context.request.session.BoundItems = {};
+        context.session.ViewModel = {};
         if (routeModule.InitializeViewModelState)
         {
             console.log("Initializing view model state (on nav)");
-            context.request.session.BoundItems = routeModule.InitializeViewModelState(context, context.request.session);
+            context.session.ViewModel = routeModule.InitializeViewModelState(context, context.session);
         }
-        context.response.send({ "BoundItems": context.request.session.BoundItems, "View": routeModule.View });
-    }
 
-    return true;
+        context.response.Path = route;
+        context.response.View = routeModule.View
+        context.response.ViewModel = context.session.ViewModel;
+    }
 }
 navigateToView = fnNavigateToView;
 
-exports.process = function(path, request, response)
+// Takes a Maaas request object and returns a Maaas response object
+//
+exports.process = function(session, requestObject)
 {
-    if (!path)
+    var context = 
     {
-        path = "menu";
+        session: session,
+        request: requestObject,
+        response: { Type: "response", Path: requestObject.Path }
+    };
+
+    console.log("Processing path " + context.request.Path);
+
+	var routeModule = routes[context.request.Path];
+    if (routeModule)
+    {
+        var viewModelAfterUpdate = null;
+
+        console.log("Found route module for " + context.request.Path);
+
+        if (context.request.ViewModelDeltas)
+        {
+            console.log("ViewModel before deltas: " + context.session.ViewModel);
+
+            // Record the current state of view model so we can diff it after apply the changes from the client,
+            // and use that diff to see if there were any changes, so that we can then pass them to the OnChange
+            // handler (for the "view" mode, indicating changes that were made by/on the view).
+            //
+            var viewModelBeforeUpdate = JSON.parse(JSON.stringify(context.session.ViewModel));
+
+            // Now apply the changes from the client...
+            for (var i = 0; i < context.request.ViewModelDeltas.length; i++) 
+            {
+                console.log("View Model change from client - path: " + context.request.ViewModelDeltas[i].path + ", value: " + context.request.ViewModelDeltas[i].value);
+                setObjectProperty(session.ViewModel, context.request.ViewModelDeltas[i].path, context.request.ViewModelDeltas[i].value);
+            }
+            
+            // Getting this here allows us to track any changes made by server logic (in change notifications or commands)
+            //
+            viewModelAfterUpdate = JSON.parse(JSON.stringify(context.session.ViewModel));
+
+            // If we had changes from the view and we have a change listener for this route, call it.
+            if (routeModule.OnChange)
+            {
+                // !!! Pass changelist (consistent with command side changelist)
+                routeModule.OnChange(context, context.session, context.session.ViewModel, "view");
+            }
+        }
+
+		if (requestObject.Command)
+        {
+            console.log("Running command: " + context.request.Command);
+
+            // If no view model updates happened on the client, we need to record the state of the
+            // view model now, before we run any commands, so we can diff it after...
+            // (this is really "ViewModel after update from client, if any" or "ViewModel before
+            // any server code gets a crack at them).
+            //
+            if (!viewModelAfterUpdate)
+            {
+                viewModelAfterUpdate = JSON.parse(JSON.stringify(context.session.ViewModel));
+            }
+
+            routeModule.Commands[context.request.Command](context, context.session, context.session.ViewModel);
+
+            // If we have a change listener for this route, analyze changes, and call it as appropriate.
+            if (routeModule.OnChange)
+            {
+                // !!! We need to call getChangeList here also, to determine if there were any changes, and
+                //     to construct the changelist for the handler (consistend with the view side changelist).
+                routeModule.OnChange(context, context.session, context.session.ViewModel, "command");
+            }
+
+            // Only send back the view model updates is we're staying on this view (path)...
+            if (context.request.Path == context.response.Path)
+            {
+                var viewModelUpdates = objectMonitor.getChangeList(null, viewModelAfterUpdate, context.session.ViewModel);
+                context.response.ViewModelDeltas = viewModelUpdates;
+            }
+        }
+        else
+        {
+            context.session.ViewModel = {};
+            if (routeModule.InitializeViewModelState)
+            {
+                console.log("Initializing view model state");
+                context.session.ViewModel = routeModule.InitializeViewModelState(context, context.session);
+            }
+
+            context.response.View = routeModule.View;
+            context.response.ViewModel = session.ViewModel;
+        }
     }
-    console.log("API request for path: " + path);
-    response.socket.setNoDelay(true);
-    processPath(path, request, response);
+    else
+    {
+        context.response.error = "No route found for command: " + context.request.command;
+    }
+
+    return context.response;
 }
--- MaaasServer/app.js
+++ MaaasServer/app.js
@@ -13,6 +13,9 @@ var api = require('./api/api');
 
 var app = express();
 
+var MemoryStore = express.session.MemoryStore;
+var sessionStore = new MemoryStore();
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +24,7 @@ app.set('view engine', 'jade');
 app.use(express.cookieParser());
 // Note: Setting the maxAge value to 60000 (one hour) generates a cookie that .NET does not record (date generation/parsing
 // is my guess) - for now we just omit expiration...
-app.use(express.cookieSession({ secret: 'sdf89f89fd7sdf7sdf', cookie: { maxAge: false, httpOnly: true } }));
+app.use(express.cookieSession({ store: sessionStore, secret: 'sdf89f89fd7sdf7sdf', cookie: { maxAge: false, httpOnly: true } }));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.query());
@@ -40,16 +43,71 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-// We do a little massaging of the request below to remove the API prefix so that the API
-// "module" doesn't have to care where it lives in the directory/path structure.  We map
-// the API resources as well for the same reason.
-//
-app.all('/api/*', function(request, response) {
-    var path = request.path.replace(/^[/]api[/]?/, '');
-    api.process(path, request, response);
+app.post('/api', function(request, response)
+{
+    var responseObject = api.process(request.session, request.body);
+    response.socket.setNoDelay(true);
+    response.send(responseObject);
 });
 app.use('/api/resources', express.static(path.join(__dirname, 'api/resources')));
 
-http.createServer(app).listen(app.get('port'), function(){
+var WebSocket = require('faye-websocket');
+
+var server = http.createServer(app);
+
+// This is a copy of the WebSocket.isWebSocket function.  This was failing (on Azure only),
+// because the Connection: Upgrade header sent by the client (confirmed by Fiddler) was
+// getting modified by something in the Azure environment such that it showed up at this
+// point as Connection: Keep-alive.  So we will use this re-written version to skip that
+// check (it presumably qualified somehow since the server.on('upgrade') got triggered.
+//
+function isWebSocket(request) 
+{
+    if (request.method !== 'GET') 
+        return false;
+
+    var connection = request.headers.connection || '',
+        upgrade    = request.headers.upgrade || '';
+
+    return request.method === 'GET' &&
+           // !!! connection.toLowerCase().split(/\s*,\s*/).indexOf('upgrade') >= 0 &&
+           upgrade.toLowerCase() === 'websocket';
+}
+
+server.on('upgrade', function(request, socket, body) 
+{
+    if (isWebSocket(request)) // was: if (WebSocket.isWebSocket(request))
+    {
+        var ws = new WebSocket(request, socket, body);
+        console.log("Initialized WebSocket");
+
+        // !!! Fix this...
+        //
+        // This is super low tech.  Since all of the transactions on this web socket
+        // have this context, we're just going to allocate an object and use that as
+        // the "session" for all of them.  This is not a great solution (has all the
+        // downsides of MemoryStore) and doesn't provide any persistence across connections.
+        // If a websocket connection is disconnected, the client will fire up a new one,
+        // and that one won't have any session state.
+        //
+        session = {};
+
+        ws.on('message', function(event) 
+        {
+            console.log('message', event.data);
+            var requestObject = JSON.parse(event.data);
+            var responseObject = api.process(session, requestObject);
+            ws.send(JSON.stringify(responseObject));
+        });
+
+        ws.on('close', function(event) 
+        {
+            console.log('close', event.code, event.reason);
+            ws = null;
+        });
+    }
+});
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
