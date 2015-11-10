78da18333d54dbbf2bbbbb4cb94a4f16c6d89491
Robert Dickinson <bob.dickinson@gmail.com>
2015-11-09 19:33:00 -0800

    Canvas typo

--- SynchroServer/synchro-apps/samples/canvas.js
+++ SynchroServer/synchro-apps/samples/canvas.js
@@ -6,7 +6,7 @@ exports.View =
     elements:
     [
         { control: "canvas", width: "*", height: "300", contents: [
-            { control: "rectangle", width: "100", height: "100", left: "{redLeft}", top: "{redTop}s", fill: "Red", margin: 0 },
+            { control: "rectangle", width: "100", height: "100", left: "{redLeft}", top: "{redTop}", fill: "Red", margin: 0 },
             { control: "rectangle", width: "100", height: "100", left: "100", top: "100",fill: "Green", margin: 0 },
         ] },
         { control: "stackpanel", orientation: "Horizontal", contents: [
